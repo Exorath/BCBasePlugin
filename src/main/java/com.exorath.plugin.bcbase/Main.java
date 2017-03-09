f195c59297f834e36ae214cf47337149ceb324c5
Toon Sevrin <twan123@live.be>
2017-03-09 22:19:47 +0100

    The environment variables now load in

--- src/main/java/com.exorath.plugin.bcbase/Main.java
+++ src/main/java/com.exorath.plugin.bcbase/Main.java
@@ -6,7 +6,13 @@ import com.exorath.plugin.bcbase.serverInfoProvider.ConnectorAPIServerInfoProvid
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
 import net.md_5.bungee.event.EventHandler;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by toonsev on 2/27/2017.
@@ -16,12 +22,21 @@ public class Main extends Plugin implements Listener {
     private static BCBaseAPI baseAPI;
     private ServerIdProvider serverIdProvider;
 
+    private Configuration configuration;
+
     @Override
     public void onEnable() {
         Main.instance = this;
         this.serverIdProvider = new ServerUUIDProvider();
         System.out.println("BCBasePlugin: The uuid of this server is " + serverIdProvider.getServerId());
         getProxy().getPluginManager().registerListener(this, this);
+
+        try {
+            configuration = loadConfig();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     @EventHandler
@@ -45,15 +60,36 @@ public class Main extends Plugin implements Listener {
         return Main.baseAPI;
     }
 
-    private  String getLobbyGameId(){
+    private Configuration loadConfig() throws IOException {
+        File config = new File(getDataFolder(), "config.yml");
+        if (!config.exists())
+            config.createNewFile();
+        return ConfigurationProvider.getProvider(YamlConfiguration.class).load(config);
 
     }
 
-    private  String getLobbyFlavorId(){
-
+    private String getLobbyGameId() {
+        if(!configuration.contains("lobby.gameId")){
+            System.out.println("No lobby.gameId key found in config, exiting.");
+            System.exit(1);
+        }
+        return configuration.getString("lobby.gameId");
     }
 
-    private String getConnectorServiceAddress(){
+    private String getLobbyFlavorId() {
+        if(!configuration.contains("lobby.flavorId")){
+            System.out.println("No lobby.gameId key found in config, exiting.");
+            System.exit(1);
+        }
+        return configuration.getString("lobby.flavorId");
+    }
 
+    private String getConnectorServiceAddress() {
+        String address = System.getenv("CONNECTOR_SERVICE_ADDRESS");
+        if(address == null){
+            System.out.println("No CONNECTOR_SERVICE_ADDRESS environment value set");
+            System.exit(1);
+        }
+        return address;
     }
 }
